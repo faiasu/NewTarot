81eaa0698e9ff8aea16a8ff84a5545749c4a0972
福角 彰啓 <akihiro@Mac.local>
2013-10-14 17:48:50 +0900

    Initial Commit

---
+++ NewTarot/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  NewTarot
+//
+//  Created by 福角 彰啓 on 13/10/14.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
